b63fa8657abd75f9132fe75d93509375fc6d8228
com.chinaos.snaill <com.chinaos.snaill@fdcf85e8-38ba-11de-9d05-31133e6853b1>
2009-05-06 09:47:14 +0000

    增加基础类框架

--- trunk/Addin/IService.cs
+++ trunk/Addin/IService.cs
@@ -5,7 +5,38 @@ using System.Text;
 
 namespace OneShoot.Addin
 {
-    interface IService
+    public interface IService
     {
+        ITweet AddTweet(string text);
+        ITweet AddTweet(string text, double replyid);
+        void DestroyTweet(double id);
+
+        ITweet SendDirectMessage(string text, string user);
+        void DestroyDirectMessage(double id);
+
+        void FollowUser(string userName);
+
+        IUserCollection GetFriends(int userId);
+        IUserCollection GetFriends();
+        IUser GetUser(int userId);
+
+        IUser CurrentlyLoggedInUser { get; set; }
+        System.Security.SecureString Password { get; set; }
+        IUser Login();
+
+        ITweetCollection GetFriendsTimeline();
+        ITweetCollection GetFriendsTimeline(string since, string userId);
+        ITweetCollection GetFriendsTimeline(string since);
+        ITweetCollection GetPublicTimeline(string since);
+        ITweetCollection GetPublicTimeline();
+        ITweetCollection GetReplies();
+        ITweetCollection GetReplies(string since);
+
+        ITweetCollection GetUserTimeline(string userId);
+
+        ITweetCollection RetrieveMessages();
+        ITweetCollection RetrieveMessages(string since);
+
+        System.Net.IWebProxy WebProxy { get; set; }
     }
 }
--- trunk/Addin/IUser.cs
+++ trunk/Addin/IUser.cs
@@ -7,5 +7,15 @@ namespace OneShoot.Addin
 {
     public interface IUser
     {
+        int Id { get; set; }
+        string Name { get; set; }
+        string SiteUrl { get; set; }
+        string ImageUrl { get; set; }
+        string Description { get; set; }
+        string Location { get; set; }
+
+        int StatusesCount { get; set; }
+        int FollowersCount { get; set; }
+        int FollowingCount { get; set; }
     }
 }
