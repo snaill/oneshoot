91ba2a0cd12763264d876a9591afd118a60717a5
com.chinaos.snaill <com.chinaos.snaill@fdcf85e8-38ba-11de-9d05-31133e6853b1>
2009-06-17 02:38:54 +0000

    增加digu项目 增加twitter和digu的部分代码，未测试

--- Digu/Service.cs
+++ Digu/Service.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
+using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
 using System.ServiceModel.Web;
 
-namespace OneShoot.Addin.Twitter
+namespace OneShoot.Addin.Digu
 {
     /// <summary>
-    /// Twitter API 文档
-    /// http://apiwiki.twitter.com/
+    /// 嘀咕 API 文档
+    /// http://code.google.com/p/digu-api/wiki/DiguApi
     /// </summary>
     public class Service : OneShoot.Addin.IService
     {
@@ -19,31 +21,31 @@ namespace OneShoot.Addin.Twitter
         public string UserName { get; set; }
         public string Password { get; set; }
 
-        public TweetCollection GetTimeline(Timeline tl, string userId, string since, int max)
+        public TweetCollection GetTimeline( Timeline tl, string userId, string since, int max )
         {
             string url = "";
-            switch (tl)
+            switch ( tl )
             {
                 case Timeline.Friends:
                     {
-                        url = string.Format("http://twitter.com/statuses/friends_timeline.json?id={0}&count={1}&since_id={2}&page={3}",
+                        url = string.Format("http://api.digu.com/statuses/friends_timeline.json?id={0}&count={1}&since_id={2}&page={3}",
                             userId, 20, since, 1);
                     }
                     break;
                 case Timeline.User:
                     {
-                        url = string.Format("http://twitter.com/statuses/user_timeline.json?id={0}&count={1}&since_id={2}&page={3}",
+                        url = string.Format("http://api.digu.com/statuses/user_timeline.json?id={0}&count={1}&since_id={2}&page={3}",
                             userId, 20, since, 1);
                     }
                     break;
                 case Timeline.Public:
                     {
-                        url = string.Format("http://twitter.com/statuses/public_timeline.json?count={0}", 20);
+                        url = string.Format("http://api.digu.com/statuses/public_timeline.json?count={0}", 20);
                     }
                     break;
                 case Timeline.Replies:
                     {
-                        url = string.Format("http://twitter.com/statuses/replies.json?count={1}&since_id={2}&page={3}",
+                        url = string.Format("http://api.digu.com/statuses/replies.json?count={1}&since_id={2}&page={3}",
                             20, since, 1);
                     }
                     break;
@@ -83,4 +85,3 @@ namespace OneShoot.Addin.Twitter
         }
     }
 }
-
