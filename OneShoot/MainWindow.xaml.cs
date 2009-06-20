010d5bb98ba68461215061b4984ed5a2d8d2a187
com.chinaos.snaill <com.chinaos.snaill@fdcf85e8-38ba-11de-9d05-31133e6853b1>
2009-06-20 03:34:05 +0000

    添加图标版权信息 添加标题栏控件 对主窗体作相应修改

--- OneShoot/MainWindow.xaml.cs
+++ OneShoot/MainWindow.xaml.cs
@@ -12,13 +12,12 @@ namespace OneShoot
 {
     public partial class MainWindow
 	{
-        System.Windows.Forms.NotifyIcon notifyIcon;
-
 		public MainWindow()
 		{
 			this.InitializeComponent();
 			
 			// 在此点之下插入创建对象所需的代码。
+            this.Header.Owner = this;
 		}
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -71,45 +70,6 @@ namespace OneShoot
         private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
             NewGridLayout.Visibility = Visibility.Collapsed;
-        }
-
-        private void TitleBar_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-            if (e.LeftButton == System.Windows.Input.MouseButtonState.Pressed)
-                this.DragMove();
-        }
-
-        private void TitleBar_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            if (e.ClickCount > 1)
-            {
-                if (this.WindowState == WindowState.Normal)
-                {
-                    this.WindowState = WindowState.Maximized;
-                }
-                else
-                {
-                    this.WindowState = WindowState.Normal;
-                }
-            }
-        }
-
-        private void TitleBar_MouseRightButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            this.notifyIcon = new System.Windows.Forms.NotifyIcon();
-            this.notifyIcon.Click += new EventHandler(notifyIcon_Click);
-            this.notifyIcon.BalloonTipText = "One shoot still running.";
-            this.notifyIcon.Text = "One shoot still running.";
-            this.notifyIcon.Icon = new System.Drawing.Icon("Resource/OneShoot.ico");
-            this.notifyIcon.Visible = true;
-            this.Hide();
-        }
-
-        void notifyIcon_Click(object sender, EventArgs e)
-        {
-            this.notifyIcon.Dispose();
-            this.notifyIcon = null;
-            this.Show();
         }
 	}
 }
---
+++ OneShoot/TitleBarControl.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+
+namespace OneShoot
+{
+	public partial class TitleBarControl
+	{
+        System.Windows.Forms.NotifyIcon notifyIcon;
+
+		public TitleBarControl()
+		{
+			this.InitializeComponent();
+
+			// 在此点之下插入创建对象所需的代码。
+		}
+
+        public Window Owner { get; set; }
+
+        private void OnMouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (e.LeftButton == System.Windows.Input.MouseButtonState.Pressed)
+                Owner.DragMove();
+        }
+
+        private void OnMouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ClickCount > 1)
+            {
+                if (Owner.WindowState == WindowState.Normal)
+                {
+                    Owner.WindowState = WindowState.Maximized;
+                }
+                else
+                {
+                    Owner.WindowState = WindowState.Normal;
+                }
+            }
+        }
+
+        void notifyIcon_Click(object sender, EventArgs e)
+        {
+            this.notifyIcon.Dispose();
+            this.notifyIcon = null;
+            Owner.Show();
+        }
+
+        private void OnMouseRightButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon();
+            this.notifyIcon.Click += new EventHandler(notifyIcon_Click);
+            this.notifyIcon.BalloonTipText = "One shoot still running.";
+            this.notifyIcon.Text = "One shoot still running.";
+            this.notifyIcon.Icon = new System.Drawing.Icon("Resource/OneShoot.ico");
+            this.notifyIcon.Visible = true;
+            Owner.Hide();
+        }
+
+
+        private void Close_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Owner.Close();
+        }
+
+        private void Logo_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://OneShoot.org");
+        }
+	}
+}
