dd9b0cce3468323630ef092aabf65170ded5e4f8
com.chinaos.snaill <com.chinaos.snaill@fdcf85e8-38ba-11de-9d05-31133e6853b1>
2009-06-27 17:55:53 +0000

    调整结构

---
+++ Addin/HttpBasicAuth.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.ServiceModel.Web;
+
+namespace OneShoot.Addin
+{
+    public class HttpBasicAuth
+    {
+        public System.Net.IWebProxy WebProxy { get; set; }
+
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        /// <summary>
+        /// 获取
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public System.Net.WebResponse GetResponse(string url)
+        {
+            try
+            {
+                System.Net.WebRequest req = System.Net.WebRequest.Create(url);
+                System.Net.CredentialCache myCache = new System.Net.CredentialCache();
+                myCache.Add(new Uri(url), "Basic", new System.Net.NetworkCredential(UserName, Password));
+                req.Credentials = myCache;
+                req.Method = "GET";
+                req.Proxy = WebProxy;
+
+                return req.GetResponse();
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
