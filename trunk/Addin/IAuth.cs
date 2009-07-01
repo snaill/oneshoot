9b436d9a90f1540d970b019d38cbcb3e3239ba62
com.chinaos.snaill <com.chinaos.snaill@fdcf85e8-38ba-11de-9d05-31133e6853b1>
2009-07-01 04:22:37 +0000

    添加漏签入的代码 删除msi安装项目（已改用clickonce部署）

---
+++ trunk/Addin/IAuth.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OneShoot.Addin
+{
+    public interface IAuth
+    {
+        string UserName { get; set; }
+        string Password { get; set; }
+        string Service { get; set; }
+    }
+}
